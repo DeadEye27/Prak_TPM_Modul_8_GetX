29fd4bb12c4f25bda9675d9ade35eebd7932558c
AndiZirahitaf <andikafatihariz@gmail.com>
2026-04-26 13:37:49 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/modul_8_prak_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.modul_8_prak_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'routes/app_pages.dart';
+import 'routes/app_routes.dart';
+import 'view/pages/product_page.dart';
+  
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+   Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'GetX App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.products, // route awal aplikasi
+      getPages: AppPages.routes, // daftar semua route
+    );
+
+  }
+
+}
